bc041faf61e6591d88a5c3b93f722659297725a9
Jusna Begum <jb@juweez.co.uk>
2017-01-28 23:51:22 +0000

    Persist shortened URLs in a hash
    This is to allow us to redirect to the original url when a shortened url is
    visited.

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@ require 'sinatra'
 require 'json'
 require_relative './url_shortener'
 
+class Cache
+  @@shortened_urls = {}
+
+  def self.init()
+    @@shortened_urls = {}
+  end
+
+  def self.add(short_url_id, original_url)
+    @@shortened_urls[short_url_id] = original_url
+  end
+
+  def self.urls()
+    return @@shortened_urls
+  end
+end
+
+configure do
+  Cache::init
+end
+
 get '/' do
   erb :index
 end
@@ -12,6 +32,8 @@ post '/' do
   short_url = "#{request.base_url}/#{short_url_id}"
 
   if short_url
+    Cache::add(short_url_id, original_url)
+
     content_type :json
     { short_url: short_url, original_url: original_url }.to_json
   else
