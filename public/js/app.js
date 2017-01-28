76b83c0282c52a5fc4288994349be27f425e95a5
Jusna Begum <jb@juweez.co.uk>
2017-01-28 23:33:20 +0000

    Display submitted URLs in a list

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 var urlShortenerForm = document.querySelector('.js-shortener-form'),
     urlShortenerInput = document.querySelector('.js-shortener-input')
+    urlList = document.querySelector('.js-url-list'),
     postUrl = urlShortenerForm.getAttribute('action');
 
 urlShortenerForm.addEventListener("submit", function(e) {
@@ -16,8 +17,35 @@ urlShortenerForm.addEventListener("submit", function(e) {
 
   xhr.onload = function() {
     if (xhr.responseText) {
-      urlShortenerInput.value = JSON.parse(xhr.responseText).short_url;
+      var response = JSON.parse(xhr.responseText);
+
+      urlShortenerInput.value = response.short_url;
       urlShortenerInput.select();
+
+      addUrlToList(response.original_url, response.short_url);
     }
   }
 });
+
+function addUrlToList(originalUrl, shortUrl) {
+  var t = document.querySelector('#urlListTemplate'),
+      list = document.querySelector('.js-list'),
+      listItem = t.content.querySelector('li'),
+      listItemOriginal = t.content.querySelector('.js-original-url a'),
+      listItemShort = t.content.querySelector('.js-short-url a');
+
+  var allListItems = document.querySelectorAll('.js-list li');
+
+  allListItems.forEach(function(el) {
+    el.classList.remove('m-list__item--highlight');
+  });
+
+  listItemOriginal.textContent = originalUrl;
+  listItemOriginal.setAttribute('href', originalUrl);
+  listItemShort.textContent = shortUrl;
+  listItemShort.setAttribute('href', shortUrl);
+  listItem.classList.add('m-list__item--highlight');
+
+  var clone = document.importNode(t.content, true);
+  list.insertBefore(clone, list.firstChild);
+};
